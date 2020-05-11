99c728c8f7c3aea746bff77fe2d3cd72b5b59d68
maoz-grossman <maozg2008@gmail.com>
2020-05-11 13:48:57 +0300

    finish_Ex2

--- Ex2/alg.h
+++ Ex2/alg.h
@@ -22,3 +22,41 @@ InputIt Add_n(InputIt first, InputIt last, const Number n)
     }
     return first;
 }
+
+template<class InputIt>
+InputIt Transpose (InputIt start, InputIt end)
+{ 
+
+    InputIt it= start;
+    
+    for(;;)
+    {
+      it=start+1;
+      if(it==end||start==end)
+        break;
+      auto temp = *it;
+      *it=*start;
+      *start=temp;
+      start+=2;
+    }
+    return start;
+}
+
+
+template<class InputIt, class OutputIt, class BinaryOperation>
+OutputIt Transform(InputIt start, InputIt end, OutputIt dest, BinaryOperation f)
+{
+    //if it's an empty container, or has an odd length
+    if(start==end || std::distance(start,end)%2!=0)
+    {
+        return dest;
+    }
+
+    for(;start!=end;start+=2 )
+    {
+        InputIt it= start+1;
+        *dest = f(*start,*it);
+        dest++;
+    }
+    return dest;
+}
--- Ex2/main.cpp
+++ Ex2/main.cpp
@@ -3,24 +3,52 @@
 #include <string>
 #include<algorithm>
 #include<iostream>
+#include<vector>
+
+template<typename T, size_t n>
+void print_array(T const (&arr)[n])
+{
+    std::cout<< "[ ";
+    for(size_t i=0; i!=n; ++i)
+        std::cout<< arr[i] <<' ';
+    std::cout<< "]" <<std::endl;
+}
+
+int Add_Numbers(const int i, const int j)
+{
+    return (i+j);
+}
 
 int main()
 {
-    std::string space=" ";
+    std::cout<<"\n~~~~~Add_n function~~~~~"<<std::endl;
     int a[]={1,2,3,4,5,6,7,8,9};
     auto p= Add_n(std::begin(a),std::end(a),3);
-    for(int i: a)
-    {
-        std::cout<<i<<space;
-    }
-    std::cout<<"\np is pointion on: "<<*p<<std::endl;
+    print_array(a);
+    std::cout<<"p is pointing on: "<<*p<<std::endl;
    
     int ai[]={1,2,3,4,5,6,7,8,9};
      p= Add_n(std::begin(ai),std::end(ai),2);
-    for(int i: ai)
-    {
-        std::cout<<i<<space;
-    }
-    std::cout<<"\np is pointion on: "<<*p<<std::endl;
+     print_array(ai);
+    std::cout<<"p is pointing on: "<<*p<<std::endl;
+
+    std::cout<<"\n~~~~~Transpose function~~~~~"<<std::endl;
+    int ai2[]={1,2,3,4,5,6,7,8,9};
+    auto p2= Transpose(std::begin(ai2),std::end(ai2));
+     print_array(ai2);
+    std::cout<<"p is pointing on: "<<*p2<<std::endl;
+    int ai3[]={1,2,3,4,5,6,7,8,9,10};
+    p2= Transpose(std::begin(ai3),std::end(ai3));
+     print_array(ai3);
+    std::cout<<"is p==end(ai3)? "<<bool(p2==std::end(ai3))<<std::endl;
+    int ai4[]={1,2,3};
+    p2= Transpose(std::begin(ai4),std::end(ai4));
+    print_array(ai4);
+    std::cout<<"p is pointing on: "<<*p2<<std::endl;
+    
+    std::cout<<"\n~~~~~Transform function~~~~~"<<std::endl;
+    int ai5[]={1,2,3,10,8,6};
+    std::vector<int>vec(100);
+    auto p3= Transform(std::begin(ai5), std::end(ai5),vec.begin(),[](const int i, const int j){ return i+j;});
     return 0;
 }
